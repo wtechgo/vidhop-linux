5c4303f7f1e41bd7e8537e94ff3cca2de0b66674
wtechgo <wtechgo@proton.me>
2022-09-04 19:47:25 +0200

    Fix install script. Add uninstaller.
    Signed-off-by: wtechgo <wtechgo@proton.me>

--- install.sh
+++ install.sh
@@ -18,44 +18,47 @@ echo -e "ATTENTION: This script is written for Arch like distros.n"
 echo -e "           Modify the script accordingly if you have another distro.\n"
 echo -n "When you are ready, press enter: " && read
 
-
 echo "installing required packages from Linux repositories..."
 # mediainfo nano openssh git ncurses moreutils python python-pip ffmpeg jq
-sudo pacman -S install mediainfo   # required for `specs`
-sudo pacman -S install nano  # for editing code with nanodlv, nanofvid,...
-sudo pacman -S install openssh # install ssh client and server (sshd command)
-sudo pacman -S install git # pull in code and updates
-sudo pacman -S install ncurses # for installing tput, used in fvid
-sudo pacman -S install moreutils # for fetching the current IP address
-sudo pacman -S install python ffmpeg
+sudo pacman --noconfirm -S mediainfo   # required for `specs`
+sudo pacman --noconfirm -S nano  # for editing code with nanodlv, nanofvid,...
+sudo pacman --noconfirm -S openssh # install ssh client and server (sshd command)
+sudo pacman --noconfirm -S git # pull in code and updates
+sudo pacman --noconfirm -S ncurses # for installing tput, used in fvid
+sudo pacman --noconfirm -S moreutils # for fetching the current IP address
+sudo pacman --noconfirm -S python
+sudo pacman --noconfirm -S ffmpeg
+sudo pacman --noconfirm -S jq
 
-
-echo "installing yt-dlp packages..."
+echo "installing Python packages..."
 pip install -U pip
 pip install -U wheel
 pip install -U yt-dlp
-
-
-echo "installing VidHop packages..."
-pkg install jq
 pip install -U requests
 pip install -U selenium
 pip install -U beautifulsoup4
 pip install -U image
 pip install -U pillow
 
-
 echo "installing VidHop..."
-vidhop_app_dir="/opt/vidhop" # $PREFIX points to /data/data/com.termux/files/usr
+vidhop_app_dir="/opt/vidhop"        # $PREFIX points to /data/data/com.termux/files/usr
 loader="$vidhop_app_dir/bin/loader" # loader in /opt/vidhop
-loader_bin="/usr/local/bin/vidhop" # loader in /bin
+loader_bin="/usr/local/bin/vidhop"  # loader in /bin
 
-git clone https://github.com/wtechgo/vidhop-android.git "$vidhop_app_dir"
-
+if [ -d "$vidhop_app_dir" ]; then
+  echo -n "$vidhop_app_dir already existis, remove it? Y/n: " && read answer && answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+  [ "$answer" = "y" ] || [ -z "$answer" ] && unset answer &&
+    rm -rf "$vidhop_app_dir" && sleep 3 &&
+    git clone https://github.com/wtechgo/vidhop-linux.git "$vidhop_app_dir"
+else
+  git clone https://github.com/wtechgo/vidhop-linux.git "$vidhop_app_dir"
+fi
+chmod +x "$vidhop_app_dir/install.sh"
 chmod +x "$loader"
-cp "$loader" "$loader_bin"  # copy loader to /bin as 'vidhop' to enable running `source vidhop`
-echo -e "\n. vidhop >> $HOME/.bashrc"
-. vidhop  # loads vidhop by sourcing $PREFIX/bin/vidhop ## only works when they source install.sh
+
+sudo cp "$loader" "$loader_bin" # copy loader to /bin as 'vidhop' to , load VidHop with command 'source vidhop`
+echo -e "\n. vidhop" >>"$HOME/.bashrc"
+. vidhop # loads vidhop by sourcing $PREFIX/bin/vidhop ## only works when they source install.sh
 cd "$vidhop_dir"
 
 echo -e "\nExtra information:\n"
