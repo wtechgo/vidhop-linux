1978204429e86015e957a33fef5be1fb8026bf9f
wtechgo <wtechgo@proton.me>
2022-10-16 15:15:47 +0200

    Add scraper files. Fix loader error.
    Signed-off-by: wtechgo <wtechgo@proton.me>

--- bin/facebookscraper.py
+++ bin/facebookscraper.py
@@ -1,46 +1,17 @@
 import argparse
 import json
 import warnings
-from pprint import pprint
-
-from imagescraper import ImageScraper
-# from scrapers.image_scraper import ImageScraper
-from facebook_scraper import get_posts, exceptions as scraper_exceptions, get_photos
+from facebook_scraper import get_posts
 
 
 # https://github.com/kevinzg/facebook-scraper
 # https://github.com/bisguzar/twitter-scraper
 class FacebookScraper:
-    dest_dir = None
-    cookies_txt = "/data/code/vidhop/scrapers/cookies/facebook_cookies.txt"
-    cookies_json = "/data/code/vidhop/scrapers/cookies/facebook_cookies.json"
-    domain_cookies_json = "/data/code/vidhop/scrapers/cookies/facebook_domain_cookies.json"
-
-    def __init__(self, dest_dir_abs_path: str = None) -> None:
-        super().__init__()
-        self.dest_dir = dest_dir_abs_path
-
     def should_redirect(self, url: str):
         if 'facebook.com/photo/?fbid' in url:
             return True
         else:
             return False
-
-    def scrape_images(self, post: dict):
-        img_urls = post.get('images', None)
-        if img_urls is None:
-            print("no images in post, abort image scrape")
-            return
-        if self.dest_dir is None:
-            print("no target directory defined for images, abort image scrape")
-            return
-        try:
-            print("post has images, trying to fetch images...")
-            image_scraper = ImageScraper(self.dest_dir)
-            image_scraper.scrape_urls(img_urls)
-            print("Images saved!")
-        except PermissionError as error:
-            print(f"{str(error)}, abort image scrape")
 
     def silence_warning(self):
         # Suppresses the following 2 warnings.
@@ -52,32 +23,24 @@ class FacebookScraper:
 
     def fetch_post(self, url: str):
         self.silence_warning()
-        try:
-            # posts_gen = get_posts(post_urls=[url], cookies=self.domain_cookies_json, options={"comments": True})
-            posts_gen = get_posts(
+        posts = list(
+            get_posts(
                 post_urls=[url],
-                options={"comments": True},
-            )  # cookies_txt  cookies_json  domain_cookies_json
-            post = next(posts_gen)
-            # if self.should_redirect(url):
-            #     post_url = post.get('post_url', None)
-            #     self.fetch_post(post_url)
-            #     return
-            # self.scrape_images(post)
+                options={"comments": True}
+            )
+        )
+        post = posts[0]
+        if self.should_redirect(url):
+            post_url = post.get('post_url', None)
+            self.fetch_post(post_url)
+            return
 
-            pprint(json.dumps(post, sort_keys=True, default=str))
-        except scraper_exceptions.NotFound:
-            print(f"post not found, has it been removed?")
-            exit()
+        print(json.dumps(post, sort_keys=True, default=str))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Twitter Scraper.')
-    parser.add_argument('-u', '--url', help='the url of the post', required=True)
-    parser.add_argument('-d', '--dir', help='the dir of the post', required=False)
+    parser.add_argument('-u', '--url', help='the url of the tweet', required=True)
     args = parser.parse_args()
-    if args.dir is not None:
-        scraper = FacebookScraper(args.dir)
-    else:
-        scraper = FacebookScraper()
+    scraper = FacebookScraper()
     scraper.fetch_post(args.url)
--- bin/twitterscraper.py
+++ bin/twitterscraper.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-
 import snscrape.modules.twitter as sntwitter
 from snscrape.base import ScraperException
 
