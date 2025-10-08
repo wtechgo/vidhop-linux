196b73ad64ba382fa500493617f3d64e107ad3bb
wtechgo <wtechgo@proton.me>
2025-10-08 11:16:59 +0200

    Fix Linux install script.

--- install.sh
+++ install.sh
@@ -34,16 +34,16 @@ echo -e ""
 echo -n "When you are ready, press ENTER to continue, or CTRL-C to abort: " && read
 
 echo "installing required packages from Linux repositories..."
-sudo pacman --noconfirm -S mediainfo   # required for `specs`
-sudo pacman --noconfirm -S imagemagick # convert images
-sudo pacman --noconfirm -S nano  # for editing code with nanodlv, nanofvid,...
-sudo pacman --noconfirm -S openssh # install ssh client and server (sshd command)
-sudo pacman --noconfirm -S git # pull in code and updates
-sudo pacman --noconfirm -S ncurses # for installing tput, used in fvid
-sudo pacman --noconfirm -S moreutils # for fetching the current IP address
-sudo pacman --noconfirm -S python
-sudo pacman --noconfirm -S ffmpeg
-sudo pacman --noconfirm -S jq
+sudo pacman --noconfirm -S mediainfo    # required for `specs`
+sudo pacman --noconfirm -S imagemagick  # convert images
+sudo pacman --noconfirm -S nano         # for editing code with nanodlv, nanofvid,...
+sudo pacman --noconfirm -S openssh      # install ssh client and server (sshd command)
+sudo pacman --noconfirm -S git          # pull in code and updates
+sudo pacman --noconfirm -S ncurses      # for installing tput, used in fvid
+sudo pacman --noconfirm -S moreutils    # for fetching the current IP address
+sudo pacman --noconfirm -S jq           # vidhop dependency: JSON processor
+sudo pacman --noconfirm -S python       # yt-dlp dependency
+sudo pacman --noconfirm -S ffmpeg       # yt-dlp dependency
 
 echo "installing Python packages..."
 # https://peps.python.org/pep-0668/?ref=itsfoss.com
@@ -58,60 +58,39 @@ pip install --break-system-packages -U beautifulsoup4
 pip install --break-system-packages -U image
 pip install --break-system-packages -U pillow
 
-#yes | pip uninstall --break-system-packages wheel
-#yes | pip uninstall --break-system-packages yt-dlp
-#yes | pip uninstall --break-system-packages requests
-#yes | pip uninstall --break-system-packages selenium
-#yes | pip uninstall --break-system-packages beautifulsoup4
-#yes | pip uninstall --break-system-packages image
-#yes | pip uninstall --break-system-packages pillow
-
-## Create and use a venv for VidHop.
-## https://peps.python.org/pep-0668/?ref=itsfoss.com
-#echo "installing Python packages..."
-#python -m venv /opt/vidhop/bin/.env-python
-#source /opt/vidhop/bin/.env-python/bin/activate
-#/opt/vidhop/bin/.env-python/bin/python -m pip install --upgrade pip
-#/opt/vidhop/bin/.env-python/bin/pip install yt-dlp
-#/opt/vidhop/bin/.env-python/bin/pip install requests
-#/opt/vidhop/bin/.env-python/bin/pip install selenium
-#/opt/vidhop/bin/.env-python/bin/pip install beautifulsoup4
-#/opt/vidhop/bin/.env-python/bin/pip install image
-#/opt/vidhop/bin/.env-python/bin/pip install pillow
-## Ignore venv system: sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
-## Revert venv system: sudo touch /usr/lib/python3.11/EXTERNALLY-MANAGED
 
 echo "installing VidHop..."
-vidhop_app_dir="/opt/vidhop"        # $PREFIX points to /data/data/com.termux/files/usr
-loader="$vidhop_app_dir/bin/loader" # loader in /opt/vidhop
-loader_bin="/usr/local/bin/vidhop"  # loader in /bin
-shell_rc="$(echo $SHELL | sed "s#/bin/#$HOME/.#g")"
+vidhop_app_dir="/opt/vidhop"
+loader="$vidhop_app_dir/bin/loader"
+loader_bin="/usr/local/bin/vidhop"
+
+[ -f "$HOME/.bashrc" ] && shell_rc="$HOME/.bashrc"
+[ -f "$HOME/.zshrc" ] && shell_rc="$HOME/.zshrc"
 
 if [ -d "$vidhop_app_dir" ]; then
   echo -n "$vidhop_app_dir already existis, remove it? Y/n: " && read answer && answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
   [ "$answer" = "y" ] || [ -z "$answer" ] && unset answer &&
-    rm -rf "$vidhop_app_dir" && sleep 3 &&
-    git clone https://github.com/wtechgo/vidhop-linux.git "$vidhop_app_dir"
-else
-  git clone https://github.com/wtechgo/vidhop-linux.git "$vidhop_app_dir"
+    sudo rm -rf "$vidhop_app_dir" &&
 fi
-chmod +x "$vidhop_app_dir/install.sh"
-chmod +x "$loader"
 
-sudo cp "$loader" "$loader_bin" # copy loader to /bin as 'vidhop' to , load VidHop with command 'source vidhop`
+sudo git clone https://github.com/wtechgo/vidhop-linux.git "$vidhop_app_dir"
+sudo chown -R root:"$USER" "$vidhop_app_dir"
+sudo chmod +x "$vidhop_app_dir/install.sh"
+sudo chmod +x "$loader"
+
+sudo ln -s "$loader" "$loader_bin"
 echo -e "\n. vidhop" >>"$shell_rc"
-. vidhop # loads vidhop by sourcing $PREFIX/bin/vidhop ## only works when they source install.sh
+. vidhop # loads vidhop by sourcing (executing) /opt/vidhop/bin/loader
 cd "$vidhop_dir"
 
 echo -e "\nExtra information:\n"
-echo -e "Installation added a line to .bashrc to load VidHop in each terminal which is recommended."
+echo -e "Installation added a line to .<YOUR_SHELL>rc to load VidHop in each terminal, which is recommended."
 echo -e "It won't bog down terminal load times as VidHop is extremely lightweight."
-echo -e "The app only defines functions and variables: work only happens when YOU run a functions.\n"
+echo -e "The app only defines functions and variables: work only happens when YOU run functions.\n"
 
 echo -e "In case you don't like that, remove this line:"
 echo -e ". vidhop"
-echo -e "from your .rc file at: $shell_rc"
-echo -e "with shortucut:\n  nanobashrc\n"
+echo -e "from .<YOUR_SHELL>rc file at: $shell_rc"
 
 echo -e "You can still load VidHop manually with commands:"
 echo -e "  source vidhop" && echo "  . vidhop"
